9ea1711fa862a57e74acca1a7edacb8ce2279839
gyandeep <gyandeepsharma007@gmail.com>
2023-03-24 03:18:17 +0530

    code added to generate apidoc

--- src/main/java/com/gyan/msdesign/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/gyan/msdesign/CurrencyExchangeServiceApplication.java
@@ -1,76 +1,47 @@
 package com.gyan.msdesign;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
-import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
-import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
-import org.springframework.boot.actuate.endpoint.web.*;
-import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
-import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
-import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-@EnableWebMvc
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 
-    ObjectMapper mapper = new ObjectMapper();
     public static void main(String[] args) {
         SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
     }
 
-    @Bean
-    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
-                                                                         ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
-                                                                         EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
-                                                                         WebEndpointProperties webEndpointProperties, Environment environment) {
-        List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
-        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
-        allEndpoints.addAll(webEndpoints);
-        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
-        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
-        String basePath = webEndpointProperties.getBasePath();
-        EndpointMapping endpointMapping = new EndpointMapping(basePath);
-        boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
-                basePath);
-        return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
-                corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
-                shouldRegisterLinksMapping, null);
-    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() throws IOException {
+        System.out.println("getting apidoc start");
+        // Create a neat value object to hold the URL
+        URL url = new URL("http://localhost:8011/v3/api-docs/");
 
-    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
-                                               String basePath) {
-        return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
-                || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
-    }
+// Open a connection(?) on the URL(??) and cast the response(???)
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-    @PostConstruct
-    public void saveRestDoc() throws IOException, UnirestException {
-        Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.get("http://localhost:8011/v2/api-docs")
-                .asString();
+// Now it's "open", we can set the request method, headers etc.
+        connection.setRequestProperty("accept", "application/json");
 
+// This line makes the request
+        InputStream responseStream = connection.getInputStream();
 
-         //   String docs = response.getBody();
-            mapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/resources/rest-doc.json"),response);
+// Manually converting the response body InputStream to APOD using Jackson
+        ObjectMapper mapper = new ObjectMapper();
+     //   String apod = mapper.readValue(responseStream, String.class).toString();
 
+     JsonNode data= mapper.readTree(responseStream);
+     String apod =mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
+// Finally we have the response
+        System.out.println(apod);
     }
 }
-
